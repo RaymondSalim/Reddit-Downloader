4c207b02d274baa4bc1c233a6905ba86ecc6b3b8
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-07-04 11:17:56 +0700

    - Moved thread download to new method - Adjusted size of Dialog

--- app/src/main/java/com/raymond/redditdownloader/MainActivity.java
+++ app/src/main/java/com/raymond/redditdownloader/MainActivity.java
@@ -1,23 +1,34 @@
 package com.raymond.redditdownloader;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 
 
 public class MainActivity extends AppCompatActivity {
+    public String saveDir;
+    TextView url;
+    final Context context = this;
+    public Dialog downloadDialog;
+    private Thread download;
 
 
     @Override
@@ -26,37 +37,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         final redditDownloader redditDownloader = new redditDownloader(this);
+        TextView url = findViewById(R.id.urlDownload);
 
 
-        Button download = findViewById(R.id.download);
-        final TextView textView = findViewById(R.id.url);
-        download.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Thread download = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            redditDownloader.download(textView.getText().toString());
-                        } catch (IOException | InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-                download.start();
-
-            }
-        });
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Dialog to request URL
+                downloadDialog = new Dialog(context);
+                downloadDialog.setContentView(R.layout.downloadalertdialog);
+                downloadDialog.setTitle("Download");
+
+
+                final Button downloadButton = (Button) downloadDialog.findViewById(R.id.downloadButton);
+                final TextView url = (TextView) downloadDialog.findViewById(R.id.urlDownload);
+                downloadButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        downloadButton.setEnabled(false);
+                        Log.d("url", url.getText().toString());
+                        download(url.getText().toString(), "name1231.mp4");
+                    }
+                });
+                downloadDialog.show();
+                WindowManager.LayoutParams lp = new WindowManager.LayoutParams(); // TODO! Fix Dialog size
+                lp.copyFrom(downloadDialog.getWindow().getAttributes());
+                lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+                lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+                downloadDialog.getWindow().setAttributes(lp);
             }
         });
+    }
+
+
+    private void download(final String url, final String fileName) {
+        // Runs on a thread to prevent error
+        download = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    redditDownloader.download(url, fileName);
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        download.start();
     }
 
 }
