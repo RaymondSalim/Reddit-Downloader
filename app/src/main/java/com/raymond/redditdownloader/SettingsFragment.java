4c891a121f2701e4b450ea33c71db9104cce36ce
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-07-12 00:14:14 +0700

    - Added preference for theme - Added setTheme() to set the theme

--- app/src/main/java/com/raymond/redditdownloader/SettingsFragment.java
+++ app/src/main/java/com/raymond/redditdownloader/SettingsFragment.java
@@ -2,12 +2,38 @@ package com.raymond.redditdownloader;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        final Preference theme = findPreference("theme");
+        theme.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String themeSetting = newValue.toString();
+                setTheme(themeSetting);
+                return true;
+            }
+        });
+    }
+
+    public void setTheme(String themeSetting) {
+        switch (themeSetting) {
+            case "auto":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case "dark":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case "light":
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+        }
     }
 }
