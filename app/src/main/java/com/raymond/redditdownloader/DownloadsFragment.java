5ad8fe83d61f1160520cc2bfb4faad7d0639ebdd
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-07-18 23:48:25 +0700

    - Added recyclerview to fragment - Uses StaggeredGridLayout to show downloaded media - Added prepareData() to scan for existing files

--- app/src/main/java/com/raymond/redditdownloader/DownloadsFragment.java
+++ app/src/main/java/com/raymond/redditdownloader/DownloadsFragment.java
@@ -1,30 +1,37 @@
 package com.raymond.redditdownloader;
 
 import android.app.Dialog;
-import android.content.Context;
+import android.content.ContextWrapper;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DownloadsFragment extends Fragment {
     public Dialog downloadDialog;
     private Thread download;
-    // public TextView url;
-    // public Button downloadButton;
+    private RecyclerView recyclerView;
+    private ImageRecyclerViewAdapter mAdapter;
+    public StaggeredGridLayoutManager layoutManager;
+    public ArrayList imageDirList;
 
 
     @Override
@@ -39,6 +46,19 @@ public class DownloadsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         final redditDownloader redditDownloader = new redditDownloader(((MainActivity)getContext()));
+
+
+
+        imageDirList = prepareData();
+        Log.d("imDirList", String.valueOf(imageDirList.size()));
+
+        layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView = view.findViewById(R.id.RecyclerViewImage);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new ImageRecyclerViewAdapter(getContext(), imageDirList);
+        recyclerView.setAdapter(mAdapter);
+
 
         // FAB
         FloatingActionButton fab = view.findViewById(R.id.fab);
@@ -85,6 +105,25 @@ public class DownloadsFragment extends Fragment {
             }
         });
         download.start();
+    }
+
+    private ArrayList prepareData() {
+        ContextWrapper contextWrapper = new ContextWrapper(getContext());
+        File directory = contextWrapper.getExternalFilesDir(null);
+
+        File[] files = directory.listFiles();
+
+        ArrayList mediaDirList = new ArrayList<>();
+        for (int i = 0; i < files.length; i++) {
+            MediaObjects mediaDir = new MediaObjects();
+            mediaDir.setMediaPath(files[i].getAbsolutePath());
+            mediaDirList.add(mediaDir);
+        }
+        return mediaDirList;
+    }
+
+    public void notifyAdapter() {
+        mAdapter.notifyDataSetChanged();
     }
 
 }
