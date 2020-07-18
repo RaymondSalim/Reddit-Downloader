ad3b38b6241cdf0affb10d05291a9d293b77bbc8
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-07-18 23:50:01 +0700

    - Added popupmenu to give user the option to copy link to clipboard

--- app/src/main/java/com/raymond/redditdownloader/RecyclerViewAdapter.java
+++ app/src/main/java/com/raymond/redditdownloader/RecyclerViewAdapter.java
@@ -1,10 +1,22 @@
 package com.raymond.redditdownloader;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,14 +24,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.LinkedList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
-    private LinkedList<String> mData;
+    private LinkedList<String> mDataString;
     private LayoutInflater layoutInflater;
+
 
     // data is passed into the constructor
     public RecyclerViewAdapter(Context context, LinkedList<String> data) {
         this.layoutInflater = LayoutInflater.from(context);
-        this.mData = data;
+        this.mDataString = data;
     }
+
 
 
 
@@ -27,27 +41,62 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View mItemView = layoutInflater.inflate(R.layout.recyclerview_row, parent, false);
         return new ViewHolder(mItemView, this);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
-        String mCurrent = mData.get(position);
-        holder.wordItemView.setText(mCurrent);
+            String mCurrent = mDataString.get(position);
+            holder.wordItemView.setText(mCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        return mDataString.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public final TextView wordItemView;
+        public final ImageButton moreVert;
         final RecyclerViewAdapter mAdapter;
 
-        public ViewHolder(View itemView, RecyclerViewAdapter adapter) {
+        public ViewHolder(final View itemView, RecyclerViewAdapter adapter) {
             super(itemView);
-            wordItemView = itemView.findViewById(R.id.word );
+            wordItemView = itemView.findViewById(R.id.word);
+            moreVert = itemView.findViewById(R.id.moreVert);
             this.mAdapter = adapter;
+
+            moreVert.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onClick(View v) {
+                    PopupMenu popupMenu = new PopupMenu(moreVert.getContext(), itemView);
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.copyLink:
+                                    ClipboardManager clipboardManager = (ClipboardManager) moreVert.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                                    ClipData clip = ClipData.newPlainText("url", "https://www.reddit.com" + wordItemView.getText());
+                                    clipboardManager.setPrimaryClip(clip);
+                                    Toast.makeText(moreVert.getContext(), "URL copied to clipboard!", Toast.LENGTH_SHORT).show();
+                                    return true;
+
+                            }
+                            return false;
+                        }
+                    });
+                    popupMenu.inflate(R.menu.popup);
+                    popupMenu.setGravity(Gravity.RIGHT);
+                    popupMenu.setForceShowIcon(true);
+                    popupMenu.show();
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+            int mPosition = getLayoutPosition(); //  The position of the element clicked
 
         }
     }
