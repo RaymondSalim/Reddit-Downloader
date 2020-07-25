ea396011cb6b8c0ab8e2a0127eb52ac601e86c41
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-07-25 12:50:30 +0700

    - SettingsFragment now uses custom layout from fragment_settings.xml

--- app/src/main/java/com/raymond/redditdownloader/SettingsFragment.java
+++ app/src/main/java/com/raymond/redditdownloader/SettingsFragment.java
@@ -1,9 +1,16 @@
 package com.raymond.redditdownloader;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -12,7 +19,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        addPreferencesFromResource(R.xml.root_preferences);
 
         final Preference theme = findPreference("theme");
         theme.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
@@ -39,5 +46,16 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 getView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR); // Sets windowLightStatusBar = true
                 break;
         }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackgroundColor(ContextCompat.getColor(getContext() ,R.color.colorBackground));
+
+
+
+
+        return view;
     }
 }
