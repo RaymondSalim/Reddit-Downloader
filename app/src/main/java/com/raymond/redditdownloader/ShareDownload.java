06d16aef89495d870761e8662d66cb21ee58b0f9
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-07-25 12:51:27 +0700

    - New ShareDownload activity that receives share intent and immediately starts share Intent to share downloaded File

---
+++ app/src/main/java/com/raymond/redditdownloader/ShareDownload.java
@@ -0,0 +1,67 @@
+package com.raymond.redditdownloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ShareDownload extends AppCompatActivity {
+    private File outputFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_download);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        redditDownloader downloader = new redditDownloader(this, true);
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equalsIgnoreCase(type)) {
+                try {
+                    handleSendText(intent);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    void handleSendText(Intent intent) throws IOException, InterruptedException {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        if (sharedText != null) {
+            outputFile = redditDownloader.download(sharedText);
+        }
+        finish();
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+
+        // To share file
+        Intent shareIntent = new Intent();
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setType("image/* video/*");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider",outputFile));
+        startActivity(Intent.createChooser(shareIntent, "Share Media"));
+    }
+}
